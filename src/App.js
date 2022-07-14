a844cbe4cc777eb149abda2a7e57b774f7bf32dd
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-14 20:38:24 +0200

    Esercizio completato e funzionante

--- src/App.js
+++ src/App.js
@@ -8,12 +8,28 @@ function App() {
   const [result, setResult] = useState("")
   /* per operatori */
   const operators = ['/', '*', '+', '-', '.'];
+
+
   /* Arrow Function per l'update del calcolo */
   const updateCalc = value => {
+    /* Aggiungo una condizione */
+    if (operators.includes(value) && calc === '' ||
+      operators.includes(value) && operators.includes(calc.slice(-1))
+    ) {
+      return;
+    }
+
     /* Quindi la funzione setCalc chiamata sopra */
     setCalc(calc + value);
-  }
+    /* Verifica del Value in console */
+    console.log(`Questo è il value nella A Function updateCalc ${value}`);
 
+    /* Condizione per operatori */
+    if (!operators.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+
+  }
 
   /* Costante per i numeri */
   const createDigits = () => {
@@ -27,8 +43,27 @@ function App() {
           onClick={() => updateCalc(i.toString())} key={i}>{i}</button>
       )
     }
+    /* Verifica in console log di Digits */
+    console.log("Questo è la verifica di Digits dopo ogni push");
+    console.log(digits);
     /* Eseguo il return almeno di Digits */
     return digits;
+  }
+
+  /* Condizione per tasto = per mostrare a schermo */
+  const calculate = () => {
+    setCalc(eval(calc).toString())
+  }
+
+  /* Condizione per tasto DEL e refresh del calcolo */
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+    /* Al value lascio un metodo per il calc */
+    const value = calc.slice(0, -1);
+    console.log(`Verifica del Console Log di deleteLast ${value}`);
+    setCalc(value);
   }
 
 
@@ -39,7 +74,7 @@ function App() {
         {/* Display Calcolatrice */}
         <div className="display">
           {/* Se il risultato è 0 allora in parte al result non lasci niente */}
-          {result ? <span>(0)</span> : ''}
+          {result ? <span>({result})</span> : ''}
           {calc || '0'}
         </div>
         {/* Operatori +/-/:/* */}
@@ -53,7 +88,7 @@ function App() {
           {/* Sottrazione */}
           <button onClick={() => updateCalc('-')}>-</button>
           {/* Delete Button */}
-          <button>DEL</button>
+          <button onClick={deleteLast}>DEL</button>
         </div>
         {/* Pulsanti Calcolatrice*/}
         <div className="digits">
@@ -61,7 +96,7 @@ function App() {
           {createDigits()}
           <button onClick={() => updateCalc('0')}>0</button>
           <button onClick={() => updateCalc('.')}>.</button>
-          <button onClick={() => updateCalc('=')}>=</button>
+          <button onClick={calculate}>=</button>
         </div>
       </div>
     </div>
